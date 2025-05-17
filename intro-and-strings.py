c74e32480e3ef03b3ae4d41ad21fbef90e9b0e2a
Ahmad Imran <ahmadimran67208@gmail.com>
2025-05-17 14:01:52 +0500

    Intro to strings

---
+++ intro-and-strings.py
@@ -0,0 +1,38 @@
+# Write first ever python program.
+# print('Hello world')
+
+message = "Hello World."
+
+# Printing the stored message variable.
+# print(message)
+
+# Checking the length of the string using len() method
+# print(len(message))
+
+# Using indexes on the string 
+# print(message[5:13])
+
+# Lowering the string using lower() method
+# print(message.lower())
+
+# Capitalizing the string using upper() method
+# print(message.upper())
+
+# Finding the character using find() method
+# print(message.find('World'))
+
+# Replacing the character using replace() method
+# print(message.replace("World", "Universe"))
+
+# Checking all the methods in the string
+# print(dir(str))
+
+# Checking all the ways to combine the string
+greeting = "Hello"
+name = "Ahmad"
+
+# my_message = greeting + ", " + name + ". Welcome!"
+
+my_message = f"{greeting}, {name}. Welcome!" # f string
+
+print(my_message)
