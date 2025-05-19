be5ac3ba421ad8636bbd6812ba5f1815e0ebd199
Ahmad Imran <ahmadimran67208@gmail.com>
2025-05-19 18:47:18 +0500

    Conditionals and Booleans Lesson

---
+++ conditionals-booleans.py
@@ -0,0 +1,33 @@
+# user = "Admin"
+# logged_in = False
+
+# if user == "Admin" or logged_in:
+#   print("Welcome.")
+# else: 
+#   print("Bad Credentials")
+
+
+# age = 23
+
+# if age >= 18:
+#   print('You are allowed to drive.')
+# elif age < 18 and age >= 16: 
+#   print('You are allowed to drive but with a guardian.')
+# else:
+#   print('You are not allowed to drive.')
+ 
+# Empty Data Type
+# num = []
+
+# if num: 
+#   print("True evaluated.")
+# else: 
+#   print("False evaluated.")
+
+# Object Identity
+# a = [1,2,3]
+# b = a
+
+# print(id(a))
+# print(id(b))
+# print(a is b)
