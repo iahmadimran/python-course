a7c992124567887b15371c8a67975667b643aa5b
Ahmad Imran <ahmadimran67208@gmail.com>
2025-05-19 13:03:29 +0500

    tuples lesson

--- list-tuples-sets.py
+++ list-tuples-sets.py
@@ -1,6 +1,14 @@
-courses = ['History', 'Math', 'Statistics', 'Computer Science']
-nums = [1, 7, 6, 3, 2, 4, 5]
+# Lists
+# courses = ['History', 'Math', 'Statistics', 'Computer Science']
+# nums = [1, 7, 6, 3, 2, 4, 5]
 
+# print(courses)
+# print(nums)
 
-print(courses)
-print(nums)
+# Tuples
+
+# tuple_1 = ('History', "Math", 'Stats')
+# tuple_2 = tuple_1
+
+# print(tuple_1)
+# print(tuple_2)
