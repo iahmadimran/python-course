de2b47e4b3626fc0f3996af0510d2dc2675a21fa
Ahmad Imran <ahmadimran67208@gmail.com>
2025-05-19 14:14:52 +0500

    Dictionaries in python

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+students = {
+  'name': 'Ahmad',
+  'age': '17',
+  'subjects': [
+    "Computer science",
+    "Statistics"
+  ]
+}
+
+for keys, values in students.items():
+  print(keys, ':', values)
