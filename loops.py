9d9ebf3bf5d7a2c111ca398c28c16f8367828c87
Ahmad Imran <ahmadimran67208@gmail.com>
2025-05-20 08:27:52 +0500

    Loops lesson today

---
+++ loops.py
@@ -0,0 +1,30 @@
+# For loop
+# nums = [1, 2, 3, 4, 5]
+
+# for num in nums: 
+#   if num == 4:
+#     print('Found the number')
+#     continue
+#   print(num)
+
+# Nested for loop 
+
+# for num in nums:
+#   for letter in 'ahmad':
+#     print(num, letter)
+
+# For Range loop 
+def stars_triangles(n):
+  for i in range(1, n + 1):
+    print("*" * i)
+
+stars_triangles(6)
+
+# While loop
+# a = 4
+
+# while a <= 20:
+#   if a % 2 == 0:
+#     print(a)
+#   a += 1
+
