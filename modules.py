b3fca7264f2fa476378bdf19431e6081c15755f8
Ahmad Imran <ahmadimran67208@gmail.com>
2025-07-02 08:49:01 +0500

    imported calendar module

--- modules.py
+++ modules.py
@@ -3,8 +3,14 @@
 # import random
 # import math
 # import antigravity
+# import calendar
 
 courses = ["Maths", "Physics", "Computer Science", "Statistics"]
+
+# cal = calendar.month(2030, 5)
+# print(cal)
+
+# print(calendar.isleap(2028))
 
 # print(math.sin(math.radians(90)))
 # print(math.log(4, 4))
