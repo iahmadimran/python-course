47d9d4690117130227db7f37375c78ebfd2d5618
Ahmad Imran <ahmadimran67208@gmail.com>
2025-07-02 08:10:24 +0500

    imported random module

--- modules.py
+++ modules.py
@@ -1,10 +1,14 @@
-import my_module as module
-import sys
+# import my_module as module
+# import sys
+import random
 
 courses = ["Maths", "Physics", "Computer Science", "Statistics"]
 
-index = module.find_index(courses, "Maths")
-print(index)
-print(module.name)
+# random_course = random.choice(courses)
+# print(random_course)
 
-print(sys.path)
+# index = module.find_index(courses, "Maths")
+# print(index)
+# print(module.name)
+
+# print(sys.path)
