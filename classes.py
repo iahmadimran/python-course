6862c6370151d30a36f4defe5f0a15019c477403
Ahmad Imran <ahmadimran67208@gmail.com>
2025-07-05 16:14:10 +0500

    completed beginners python

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-sports_cars = ['ferrari', 'lamborghini', 'mclaren', 'koenisegg', 'pagani', 'porsche', 'bmw', 'corvette', 'maserati']
+sports_cars = ['ferrari', 'lamborghini', 'mclaren', 'koenisegg', 'pagani', 'porsche', 'bmw', 'corvette', 'maserati', 'buggati']
 
 class Cars:
   def __init__(self, car_name, car_model):
@@ -6,16 +6,16 @@ class Cars:
     self.model = car_model
   
   def sports_car(self):
-    if (self.car == 'ferrari' or self.car =='lamborghini' or self.car =='mclaren' or self.car =='koenisegg' or self.car =='pagani' or self.car =='porsche' or self.car =='bmw' or self.car =='corvette' or self.car =='maserati') and self.model > 2020:
+    if self.car in sports_cars and self.model > 2020:
       print(f'your car which is {self.car} is very great')
-    elif (self.car == 'ferrari' or self.car =='lamborghini' or self.car =='mclaren' or self.car =='koenisegg' or self.car =='pagani' or self.car =='porsche' or self.car =='bmw' or self.car =='corvette' or self.car =='maserati') and self.model < 2020:
+    elif self.car in sports_cars and self.model < 2020:
       print(f'your car which is {self.car} is very good but you need to level up.')
     else:
       print(f'your car which is {self.car} is average because it cannot beat supercars')
       
 
-my_car = Cars('mehran', 2022)
+my_car = Cars('porsche', 2022)
 my_car.sports_car()
 
-my_friends_car = Cars("koenisegg", 2010)
+my_friends_car = Cars("buggati", 2024)
 my_friends_car.sports_car()
