21c15a103ea3470240d0deb3310a606752466732
Ahmad Imran <ahmadimran67208@gmail.com>
2025-07-13 13:11:11 +0500

    completed half binary search tree

--- dsa/binary_search_tree.py
+++ dsa/binary_search_tree.py
@@ -5,7 +5,7 @@ class BinarySearchTreeNode:
     self.right = None
 
   def add_child(self, data):
-    if self.data == data:
+    if data == self.data:
       return
     
     if data < self.data:
@@ -20,7 +20,7 @@ class BinarySearchTreeNode:
         self.right = BinarySearchTreeNode(data)
 
   def search(self, val):
-    if self.data == val:
+    if val == self.data:
       return True
 
     if val < self.data:
@@ -33,19 +33,45 @@ class BinarySearchTreeNode:
         return self.right.search(val)
       else:
         return False
+      
+  def find_min(self):
+    while self.left is not None:
+      self = self.left
+    
+    return self.data
+  
+  def find_max(self):
+    while self.right is not None:
+      self = self.right
+    
+    return self.data
+  
+  def calculate_sum(self):
+    if self.left:
+      left_sum = self.left.calculate_sum()
+    else:
+      left_sum = 0
+
+    if self.right:
+      right_sum = self.right.calculate_sum()
+    else:
+      right_sum = 0
+
+    return self.data + left_sum + right_sum
   
   def in_order_traversal(self):
     elements = []
 
     if self.left:
       elements += self.left.in_order_traversal()
-
+    
     elements.append(self.data)
 
     if self.right:
       elements += self.right.in_order_traversal()
     
     return elements
+  
   def pre_order_traversal(self):
     elements = [self.data]
 
@@ -62,28 +88,14 @@ class BinarySearchTreeNode:
 
     if self.left:
       elements += self.left.post_order_traversal()
-
+    
     if self.right:
       elements += self.right.post_order_traversal()
-
-    elements.append(self.data)
     
+    elements.append(self.data)
+
     return elements
   
-  def find_min(self):
-    while self.left is not None:
-      self = self.left
-    return self.data
-  
-  def find_max(self):
-    while self.right is not None:
-      self = self.right
-    return self.data
-  
-  def calculate_sum(self):
-    left_sum = self.left.calculate_sum() if self.left else 0
-    right_sum = self.right.calculate_sum() if self.right else 0
-    return (self.data + left_sum + right_sum)
   
 def build_tree(elements):
   root = BinarySearchTreeNode(elements[0])
@@ -93,22 +105,14 @@ def build_tree(elements):
   
   return root
 
-if __name__ == '__main__':
-  countries = ["India","Pakistan","Germany", "USA","China","India","UK","USA"]
-  countries_tree = build_tree(countries)
-
-  # print(countries_tree.in_order_traversal())
-  # print(countries_tree.search('Nigeria'))
-
-  numbers = [23, 43, 65, 12, 3, 87, 15, 6, 43]
+if __name__ == "__main__":
+  numbers = [23, 35, 7, 12, 16, 43, 67]
   numbers_tree = build_tree(numbers)
 
-  # print(numbers_tree.find_min())
-  # print(numbers_tree.find_max())
+  print(numbers_tree.calculate_sum())
 
+  # print(numbers_tree.search(655))
   # print(numbers_tree.in_order_traversal())
   # print(numbers_tree.pre_order_traversal())
-  print(numbers_tree.post_order_traversal())
-
-  print(numbers_tree.calculate_sum())
-  # print(numbers_tree.search(34))
+  # print(numbers_tree.post_order_traversal())
+  
