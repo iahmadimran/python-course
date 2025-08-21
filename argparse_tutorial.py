7f0e82db94c24b804c7294f9937da6eae502bc31
Ahmad Imran <ahmadimran67208@gmail.com>
2025-08-21 18:17:55 +0500

    updated the code

--- argparse_tutorial.py
+++ argparse_tutorial.py
@@ -1,26 +1,49 @@
-import argparse
+# import argparse
 
-if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument('number1', help='First Number')
-  parser.add_argument('number2', help='Second Number')
-  parser.add_argument('operation', help='Operation')
+# if __name__ == "__main__":
+#   parser = argparse.ArgumentParser()
+#   parser.add_argument('number1', help='First Number')
+#   parser.add_argument('number2', help='Second Number')
+#   parser.add_argument('operation', help='Operation')
 
-  args = parser.parse_args()
+#   args = parser.parse_args()
 
-  print(args.number1)
-  print(args.number2)
-  print(args.operation)
+#   print(args.number1)
+#   print(args.number2)
+#   print(args.operation)
 
-  n1 = int(args.number1)
-  n2 = int(args.number2)
-  result = None
+#   n1 = int(args.number1)
+#   n2 = int(args.number2)
+#   result = None
 
-  if args.operation == 'add':
-    result = n1 + n2
-  elif args.operation == 'subtract':
-    result = n1 - n2
-  elif args.operation == 'multiply':
-    result = n1 * n2
+#   if args.operation == 'add':
+#     result = n1 + n2
+#   elif args.operation == 'subtract':
+#     result = n1 - n2
+#   elif args.operation == 'multiply':
+#     result = n1 * n2
   
-  print(f"Result: {result}")
+#   print(f"Result: {result}")
+
+# number1 = int(input("Enter the first number: "))
+# number2 = int(input("Enter the second number: "))
+
+# n1 = int(number1)
+# n2 = int(number2)
+# operation = input("Enter the operation you want to perform: ")
+
+# result = None
+# if operation == 'add':
+#   result = n1 + n2
+# elif operation == 'subtract':
+#   result = n1 - n2
+# elif operation == 'multiply':
+#   result = n1 * n2
+# elif operation == 'division' and n2 != 0:
+#   result = n1 / n2
+# else:
+#   print("The operation you entered can't be performed")
+
+# print("The result to your expression is:", result)
+
+
